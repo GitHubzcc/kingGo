a1925e0e9cbe4561762f096629d0f32f2fba04fb
eriz <eriz@hboxs.com>
2018-03-20 11:09:45 +0800

    add pay

---
+++ src/main/java/com/payUtil/aliPay/ali.java
@@ -0,0 +1,7 @@
+package com.payUtil.aliPay;
+
+/**
+ * Created by hboxs021 on 2018/3/20.
+ */
+public class ali {
+}
---
+++ src/main/java/com/payUtil/wechatPay/wechat.java
@@ -0,0 +1,7 @@
+package com.payUtil.wechatPay;
+
+/**
+ * Created by hboxs021 on 2018/3/20.
+ */
+public class wechat {
+}
