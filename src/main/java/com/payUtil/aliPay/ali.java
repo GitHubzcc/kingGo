5e2736db6b52c2a22bf6559abc5d412dc804f3f5
eriz <eriz@hboxs.com>
2018-03-20 14:09:16 +0800

    fix

--- src/main/java/com/payUtil/aliPay/ali.java
+++ src/main/java/com/payUtil/aliPay/ali.java
@@ -2,7 +2,7 @@ package com.payUtil.aliPay;
 
 /**
  * Created by hboxs021 on 2018/3/20.
- * 支付宝支付流程、支付宝支付faceToFace流程：
+ * 支付宝支付流程、支付宝支付faceToFace流程： https://docs.open.alipay.com/api_1/alipay.trade.pay
  * 1、集成sdk 调用通用接口 https://openapi.alipay.com/gateway.do
  */
 public class ali {
--- src/main/java/com/payUtil/wechatPay/wechat.java
+++ src/main/java/com/payUtil/wechatPay/wechat.java
@@ -2,7 +2,7 @@ package com.payUtil.wechatPay;
 
 /**
  * Created by hboxs021 on 2018/3/20.
- * 微信支付流程：
+ * 微信支付流程：  https://pay.weixin.qq.com/wiki/doc/api/app/app.php?chapter=8_5
  * 1、调用登录接口，获取用户openId   /jssdk-config-locate
  * 2、商户server调用支付统一下单
  * 3、商户server调用再次签名
