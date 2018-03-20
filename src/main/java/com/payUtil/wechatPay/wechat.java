069071a3df27fedc5d79d29d30621234544dca11
eriz <eriz@hboxs.com>
2018-03-20 11:11:53 +0800

    fix

--- src/main/java/com/payUtil/wechatPay/wechat.java
+++ src/main/java/com/payUtil/wechatPay/wechat.java
@@ -2,6 +2,11 @@ package com.payUtil.wechatPay;
 
 /**
  * Created by hboxs021 on 2018/3/20.
+ * 微信支付流程：
+ * 1、调用登录接口，获取用户openId
+ * 2、商户server调用支付统一下单
+ * 3、商户server调用再次签名
+ * 4、商户server接收支付通知
  */
 public class wechat {
 }
